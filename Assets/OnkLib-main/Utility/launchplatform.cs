6f2ffc1f3cac2349a595240c547b567b73bb7761
onkalo <87790609+Onka10@users.noreply.github.com>
2023-06-19 00:31:20 +0900

    発射システムを作成

---
+++ Assets/OnkLib-main/Utility/launchplatform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class launchplatform : MonoBehaviour
+{
+    [SerializeField] fireworks obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MasterInput.I.OnSpace
+        .Subscribe(_ => Fire())
+        .AddTo(this);
+    }
+
+    // Update is called once per frame
+    void Fire()
+    {
+        obj.Go();
+    }
+}

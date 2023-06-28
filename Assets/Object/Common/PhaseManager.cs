251a179c5e5380f50728a8351cda4bc1a238c0a2
onkalo <87790609+Onka10@users.noreply.github.com>
2023-06-28 20:00:41 +0900

    v11完成

--- Assets/Object/Common/PhaseManager.cs
+++ Assets/Object/Common/PhaseManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using DG.Tweening;
 
 public class PhaseManager : Singleton<PhaseManager>
 {
@@ -17,8 +18,19 @@ public class PhaseManager : Singleton<PhaseManager>
     public IReadOnlyReactiveProperty<bool> IsAnimating => _isAnimating;
     private readonly ReactiveProperty<bool> _isAnimating = new ReactiveProperty<bool>();
 
-    
+    [SerializeField] GameObject tran;
+
     public void Play(){
+        _state.Value = GamePhase.Road;
+        StartCoroutine(WaitOneSecond());
+    }
+
+    private IEnumerator WaitOneSecond()
+    {
+        TweenFloat(0,1,1);
+        yield return new WaitForSeconds(1f);
+        TweenFloat(1,0,1);
+        yield return new WaitForSeconds(1f);
         _state.Value = GamePhase.InGame;
     }
 
@@ -30,13 +42,28 @@ public class PhaseManager : Singleton<PhaseManager>
         _isAnimating.Value  = _isAnimating.Value ? false : true;
     }
 
+
+    void TweenFloat(float minValue, float maxValue, float duration)
+    {
+        // 値を変更する対象のオブジェクトやコンポーネントを取得
+        // ここでは例として自身のRendererを使用しますが、適宜変更してください
+        Renderer renderer = tran.GetComponent<Renderer>();
+
+        // DOTweenのTweenメソッドを使用して値の変更を定義
+        // 最小値から最大値までの範囲で変化するよう設定し、指定の秒数かけて変更します
+        DOTween.To(() => minValue, value => {
+            renderer.material.SetFloat("_transition_pow", value);
+        }, maxValue, duration);
+    }
+
 }
 
 
 public enum GamePhase{
     Title,
     InGame,
-    Result
+    Result,
+    Road
 }
 
 public enum InGamePhase{
